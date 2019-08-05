944ac8448559c0135d02e3afe1d0d57bc33eaa12
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2019-08-05 10:57:10 +0200

    error mapping stick to the original FlowResult ojbect

--- uniflow-core/src/main/kotlin/io/uniflow/core/result/ResultOperators.kt
+++ uniflow-core/src/main/kotlin/io/uniflow/core/result/ResultOperators.kt
@@ -5,14 +5,14 @@ import io.uniflow.core.flow.UIState
 suspend fun <R : Any, T : Any> FlowResult<R>.map(result: suspend (R) -> T): FlowResult<T> {
     return when (this) {
         is FlowResult.Success -> success(result(this.value))
-        is FlowResult.Error -> error(message, exception)
+        is FlowResult.Error -> this
     }
 }
 
 suspend fun <R : Any, T : Any> FlowResult<R>.flatMap(result: suspend (R) -> FlowResult<T>): FlowResult<T> {
     return when (this) {
         is FlowResult.Success -> result(this.value)
-        is FlowResult.Error -> error(message, exception)
+        is FlowResult.Error -> this
     }
 }
 
@@ -46,12 +46,3 @@ suspend fun <R : Any> FlowResult<R>.mapUIState(onResult: suspend (R) -> UIState)
         is FlowResult.Error -> exception?.let { throw exception } ?: kotlin.error(message)
     }
 }
-
-//TODO hckec if needed
-//@Suppress("UNCHECKED_CAST")
-//suspend fun <R : Any> FlowResult<R>.mapUIState(onResult: suspend (R) -> UIState, onError: suspend (FlowResult<Nothing>) -> UIState): UIState {
-//    return when (this) {
-//        is FlowResult.Success -> onResult(this.value)
-//        else -> onError(this as FlowResult<Nothing>)
-//    }
-//}
