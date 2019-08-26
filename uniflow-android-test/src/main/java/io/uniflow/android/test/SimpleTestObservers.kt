057bad72a163039abddda73bcbdea04606f4f331
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2019-08-26 13:44:11 +0200

    0.6.1 - STO - lastState() & lastEvent()

--- uniflow-android-test/src/main/java/io/uniflow/android/test/SimpleTestObservers.kt
+++ uniflow-android-test/src/main/java/io/uniflow/android/test/SimpleTestObservers.kt
@@ -17,10 +17,12 @@ class TestObserver<T> : Observer<T> {
 data class TestViewObserver(private val states: TestObserver<UIState>, private val events: TestObserver<Event<UIEvent>>) {
     fun hasState(state: UIState): Boolean = states.elements.lastOrNull() == state
     fun hasState(state: UIState, index: Int): Boolean = states.elements[index] == state
+    fun lastState(): UIState? = states.elements[states.elements.lastIndex]
     fun statesCount() = states.elements.count()
     fun hasEvent(event: UIEvent) = events.elements.lastOrNull() == event
     fun hasEvent(event: UIEvent, index: Int) = events.elements[index] == event
     fun eventsCount() = events.elements.count()
+    fun lastEvent(): UIEvent? = events.elements[events.elements.lastIndex].take()
 }
 
 fun AndroidDataFlow.createTestObserver(): TestViewObserver {
