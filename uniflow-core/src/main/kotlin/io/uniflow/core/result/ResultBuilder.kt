6a64839c75d4758900aef9ca540f8811a36b9724
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2019-08-26 15:51:00 +0200

    0.6.2 - clean result builders

--- uniflow-core/src/main/kotlin/io/uniflow/core/result/ResultBuilder.kt
+++ uniflow-core/src/main/kotlin/io/uniflow/core/result/ResultBuilder.kt
@@ -1,26 +1,26 @@
 package io.uniflow.core.result
 
-suspend fun <T : Any> result(code: suspend () -> FlowResult<T>, onError: suspend (Throwable) -> FlowResult<T>): FlowResult<T> {
+suspend fun <T : Any> result(code: suspend () -> T, onError: suspend (Throwable) -> FlowResult<T>): FlowResult<T> {
     return try {
-        code()
+        flowSuccess(code())
     } catch (e: Throwable) {
         onError(e)
     }
 }
 
-suspend fun <T : Any> networkResult(errorMessage: String = "Network flowError", code: suspend () -> FlowResult<T>): FlowResult<T> {
+suspend fun <T : Any> networkResult(errorMessage: String = "Network flowError", code: suspend () -> T): FlowResult<T> {
     return try {
-        code()
+        flowSuccess(code())
     } catch (e: Throwable) {
-        FlowResult.Error(errorMessage, NetworkException("$errorMessage - $e",e))
+        FlowResult.Error(errorMessage, NetworkException("$errorMessage - $e", e))
     }
 }
 
-suspend fun <T : Any> databaseResult(errorMessage: String = "Database flowError", code: suspend () -> FlowResult<T>): FlowResult<T> {
+suspend fun <T : Any> databaseResult(errorMessage: String = "Database flowError", code: suspend () -> T): FlowResult<T> {
     return try {
-        code()
+        flowSuccess(code())
     } catch (e: Throwable) {
-        FlowResult.Error(errorMessage, DatabaseException("$errorMessage - $e",e))
+        FlowResult.Error(errorMessage, DatabaseException("$errorMessage - $e", e))
     }
 }
 
