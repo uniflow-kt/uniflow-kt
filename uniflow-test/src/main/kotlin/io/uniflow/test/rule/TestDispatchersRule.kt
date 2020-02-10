4a25ec5905b6be9503cc4d5bf433c257cd4b3d46
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2020-02-10 15:13:52 +0100

    ✨ Add custom test dispatcher config (#18)
    * ✨ Add settable config to TestDispatchersRule
    
    Defaults to the previous situation for backwards compatibility
    
    * 📝 Update documentation
    
    * 🎨 Reformat
    
    * 📝 Update documentation

--- uniflow-test/src/main/kotlin/io/uniflow/test/rule/TestDispatchersRule.kt
+++ uniflow-test/src/main/kotlin/io/uniflow/test/rule/TestDispatchersRule.kt
@@ -3,17 +3,20 @@ package io.uniflow.test.rule
 import io.uniflow.core.dispatcher.ApplicationDispatchers
 import io.uniflow.core.dispatcher.TestDispatchers
 import io.uniflow.core.dispatcher.UniFlowDispatcher
+import io.uniflow.core.dispatcher.UniFlowDispatcherConfiguration
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 /**
  * Setup Test Configuration Dispatcher
  */
-class TestDispatchersRule : TestWatcher() {
+class TestDispatchersRule(
+    private val testDispatchers: UniFlowDispatcherConfiguration = TestDispatchers()
+) : TestWatcher() {
 
     override fun starting(description: Description?) {
         super.starting(description)
-        UniFlowDispatcher.dispatcher = TestDispatchers()
+        UniFlowDispatcher.dispatcher = testDispatchers
     }
 
     override fun finished(description: Description?) {
