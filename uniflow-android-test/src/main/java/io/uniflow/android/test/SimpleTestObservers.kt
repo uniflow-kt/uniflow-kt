9ba414f80735f0f5a65c4d6cb3a08189cb15d330
ggajews <grzegorz.gajewski@nedap.com>
2020-05-13 09:46:44 +0200

    Fix android test observer comparisions (#29)
    * update CI
    
    * Fix android test observer comparisions
    
    * Fix and simplify events comparision
    
    Co-authored-by: Arnaud Giuliani <giuliani.arnaud@gmail.com>

--- uniflow-android-test/src/main/java/io/uniflow/android/test/SimpleTestObservers.kt
+++ uniflow-android-test/src/main/java/io/uniflow/android/test/SimpleTestObservers.kt
@@ -2,7 +2,6 @@ package io.uniflow.android.test
 
 import android.arch.lifecycle.Observer
 import io.uniflow.android.flow.AndroidDataFlow
-import io.uniflow.core.flow.data.Event
 import io.uniflow.core.flow.data.UIData
 import io.uniflow.core.flow.data.UIEvent
 import io.uniflow.core.flow.data.UIState
@@ -21,7 +20,7 @@ class SimpleObserver<T>(val callback: (T) -> Unit) : Observer<T> {
 class TestViewObserver {
     val values = arrayListOf<UIData>()
     val states = SimpleObserver<UIState> { values.add(it) }
-    val events = SimpleObserver<Event<UIEvent>> { values.add(it.peek()) }
+    val events = SimpleObserver<UIEvent> { values.add(it) }
 
     val lastStateOrNull: UIState?
         get() = states.values.lastOrNull()
@@ -34,14 +33,14 @@ class TestViewObserver {
     val lastValueOrNull
         get() = values.lastOrNull()
 
-    fun assertReceived(vararg any: UIData) = assert(this.values == any) { "Wrong values\nshould have [$any]\nbut was [${values}]" }
-    fun assertReceived(vararg states: UIState) = assert(this.states.values == states) { "Wrong values\nshould have [$states]\nbut was [${this.states.values}]" }
-    fun assertReceived(vararg events: UIEvent) = assert(this.events.values == events) { "Wrong values\nshould have [$events]\nbut was [${this.events.values}]" }
+    fun assertReceived(vararg any: UIData) = assert(this.values == any.toList()) { "Wrong values\nshould have [${any.toList()}]\nbut was [${values}]" }
+    fun assertReceived(vararg states: UIState) = assert(this.states.values == states.toList()) { "Wrong values\nshould have [${states.toList()}]\nbut was [${this.states.values}]" }
+    fun assertReceived(vararg events: UIEvent) = assert(this.events.values == events.toList()) { "Wrong values\nshould have [${events.toList()}]\nbut was [${this.events.values}]" }
 }
 
 fun AndroidDataFlow.createTestObserver(): TestViewObserver {
     val tester = TestViewObserver()
     dataPublisher.states.observeForever(tester.states)
-    dataPublisher.events.observeForever(tester.events)
+    dataPublisher.events.observeForever { tester.events.onChanged(it?.peek()) }
     return tester
 }
