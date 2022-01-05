92dd61c8852758e106f70c19dcf11934d11dc15f
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2022-01-05 18:09:01 +0100

    all config fixup

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./gradlew install publishToMavenLocal
+./gradlew publishToMavenLocal
 
--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./gradlew publishToMavenLocal publishMavenPublicationPublicationToSonatypeRepository --info
+./gradlew publishAllPublicationsToStagingRepository --info
